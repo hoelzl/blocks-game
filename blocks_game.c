e2a8afcbad74a52ab665a047ff0e30129384f729
Matthias Hölzl <tc@xantira.com>
2023-09-20 17:03:51 +0200

    Update to 02_blocks_game_drawing.c

--- blocks_game.c
+++ blocks_game.c
@@ -1,3 +1,4 @@
+#pragma ide diagnostic ignored "cppcoreguidelines-narrowing-conversions"
 /***********************************************************************************
  * Example based on the raylib-intro-course, with support for emscripten.
  * CMake support is based on the CMakeLists.txt file from the raylib project.
@@ -11,11 +12,44 @@
 #include <emscripten/emscripten.h>
 #endif
 
+#define PLAYER_LIVES 5
+#define BRICKS_LINES 5
+#define BRICKS_PER_LINE 20
+#define BRICKS_POSITION_Y 50
+
 typedef enum GameScreen {
     LOGO, TITLE, GAMEPLAY, ENDING
 } GameScreen;
 
-void UpdateDrawFrame(GameScreen *screen);     // Update and Draw one frame
+typedef struct Player {
+    Vector2 position;
+    Vector2 speed;
+    Vector2 size;
+    Rectangle bounds;
+    int lives;
+} Player;
+
+typedef struct Ball {
+    Vector2 position;
+    Vector2 speed;
+    float radius;
+    bool active;
+} Ball;
+
+typedef struct Brick {
+    Vector2 position;
+    Vector2 size;
+    Rectangle bounds;
+    int resistance;
+    bool active;
+} Brick;
+
+GameScreen screen = LOGO;
+Player player = {0};
+Ball ball = {0};
+Brick bricks[BRICKS_LINES][BRICKS_PER_LINE] = {0};
+
+void UpdateDrawFrame();     // Update and Draw one frame
 
 int main() {
     const int screenWidth = 800;
@@ -23,7 +57,28 @@ int main() {
 
     InitWindow(screenWidth, screenHeight, "PROJECT: BLOCKS GAME");
 
-    GameScreen screen = LOGO;
+    // Initialize player
+    player.position = (Vector2) {GetScreenWidth() / 2.0f, GetScreenHeight() - 50.0f};
+    player.speed = (Vector2) {8.0f, 8.0f};
+    player.size = (Vector2) {100, 24};
+    player.lives = PLAYER_LIVES;
+
+    // Initialize ball
+    ball.radius = 10.0f;
+    ball.active = false;
+    ball.position = (Vector2) {player.position.x + player.size.x / 2, player.position.y - ball.radius * 2};
+    ball.speed = (Vector2) {4.0f, 4.0f};
+
+    // Initialize bricks
+    for (int j = 0; j < BRICKS_LINES; ++j) {
+        for (int i = 0; i < BRICKS_PER_LINE; ++i) {
+            bricks[j][i].size = (Vector2) {(float) GetScreenWidth() / BRICKS_PER_LINE, 20};
+            bricks[j][i].position = (Vector2) {bricks[j][i].size.x * i, BRICKS_POSITION_Y + bricks[j][i].size.y * j};
+            bricks[j][i].bounds = (Rectangle) {bricks[j][i].position.x, bricks[j][i].position.y, bricks[j][i].size.x,
+                                               bricks[j][i].size.y};
+            bricks[j][i].active = true;
+        }
+    }
 
 #if defined(PLATFORM_WEB)
     emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
@@ -34,7 +89,7 @@ int main() {
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        UpdateDrawFrame(&screen);
+        UpdateDrawFrame();
     }
 #endif
 
@@ -49,18 +104,18 @@ int main() {
 //----------------------------------------------------------------------------------
 // Module Functions Definition
 //----------------------------------------------------------------------------------
-void UpdateDrawFrame(GameScreen *screen) {
+void UpdateDrawFrame() {
 
     static int framesCounter = 0;
     static int gameResult = -1;     // -1 = Game not finished, 1 = Win, 0 = Lose
     static bool gamePaused = false;
 
-    switch (*screen) {
+    switch (screen) {
         case LOGO: {
             ++framesCounter;
 
             if (framesCounter > 180) {
-                *screen = TITLE;
+                screen = TITLE;
                 framesCounter = 0;
             }
             break;
@@ -69,7 +124,7 @@ void UpdateDrawFrame(GameScreen *screen) {
             // TODO: Update TITLE screen data here!
             ++framesCounter;
             if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
-                *screen = GAMEPLAY;
+                screen = GAMEPLAY;
             }
             break;
         }
@@ -78,7 +133,7 @@ void UpdateDrawFrame(GameScreen *screen) {
                 // TODO: Gameplay logic!
             }
             if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
-                *screen = ENDING;
+                screen = ENDING;
             }
             break;
         }
@@ -88,7 +143,7 @@ void UpdateDrawFrame(GameScreen *screen) {
             ++framesCounter;
 
             if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP)) {
-                *screen = TITLE;
+                screen = TITLE;
             }
             break;
         }
@@ -97,7 +152,7 @@ void UpdateDrawFrame(GameScreen *screen) {
     BeginDrawing();
     {
         ClearBackground(RAYWHITE);
-        switch (*screen) {
+        switch (screen) {
 
             case LOGO: {
                 DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
@@ -107,19 +162,51 @@ void UpdateDrawFrame(GameScreen *screen) {
             case TITLE: {
                 DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), GREEN);
                 DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-                DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+
+                if ((framesCounter / 30) % 2) {
+                    DrawText("PRESS [ENTER] or TAP to START",
+                             GetScreenWidth() / 2 - MeasureText("PRESS [ENTER] or TAP to START", 20) / 2,
+                             GetScreenHeight() / 2 + 60, 20,
+                             DARKGRAY);
+                }
                 break;
             }
             case GAMEPLAY: {
-                DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), PURPLE);
-                DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
-                DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
+                DrawRectangleV(player.position, player.size, BLACK);
+                DrawCircleV(ball.position, ball.radius, MAROON);
+
+                // Draw bricks
+                for (int j = 0; j < BRICKS_LINES; ++j) {
+                    for (int i = 0; i < BRICKS_PER_LINE; ++i) {
+                        if (bricks[j][i].active) {
+                            if ((i + j) % 2 == 0) {
+                                DrawRectangleV(bricks[j][i].position, bricks[j][i].size, GRAY);
+                            } else {
+                                DrawRectangleV(bricks[j][i].position, bricks[j][i].size, DARKGRAY);
+                            }
+                        }
+                    }
+                }
+                // Draw GUI: player lives
+                for (int i = 0; i < player.lives; ++i) {
+                    DrawRectangle(20 + 40 * i, GetScreenHeight() - 30, 35, 10, LIGHTGRAY);
+                }
+                // Draw pause message when required
+                if (gamePaused) {
+                    // DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(RAYWHITE, 0.8f));
+                    DrawText("GAME PAUSED", GetScreenWidth() / 2 - MeasureText("GAME PAUSED", 40) / 2,
+                             GetScreenHeight() / 2 - 40, 40, GRAY);
+                }
                 break;
             }
             case ENDING: {
                 DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLUE);
                 DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
-                DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
+                if ((framesCounter / 30) % 2 == 0) {
+                    DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN",
+                             GetScreenWidth() / 2 - MeasureText("Press ENTER or TAP to RETURN to TITLE SCREEN", 20) / 2,
+                             GetScreenHeight() / 2 + 80, 20, DARKBLUE);
+                }
                 break;
             }
         }
